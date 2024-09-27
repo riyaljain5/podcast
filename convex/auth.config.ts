1cc985f4be33a36f34fa68e64f1704ddf3942bb9
Riyal Jain <69381468+riyaljain5@users.noreply.github.com>
2024-09-27 19:26:44 +0530

    made changes

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://deciding-badger-15.clerk.accounts.dev/",
+            applicationId: "convex",
+        },
+    ]
+}
+export default authConfig;
